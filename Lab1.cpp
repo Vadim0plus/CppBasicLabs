bc2bf69bc753484657beffecf24b2c7e1a89f1df
Vadim Tukhvatullin <vadim0plus@gmail.com>
2017-05-17 22:48:08 +0300

    Defining a Member Function with a Parameter

--- Lab1.cpp
+++ Lab1.cpp
@@ -1,6 +1,7 @@
 //Lab1
 //Define class GradeBook with a member function DisplayMessage
 #include <iostream>
+#include <string> // program uses C++ standard string class
 using namespace std;
 
 //GradeBook class definition
@@ -8,16 +9,23 @@ class GradeBook
 {
 public:
 	// function that display a message to the GradeBook user
-	void DisplayMessage()
+	void displayMessage(string courseName)
 	{
-		cout << "Welcome to the Grade Book!" << endl;
+		cout << "Welcome to the Grade Book\n" << courseName << "!" <<endl;
 	}
 };
 
 //	function main begins program execution
 int main()
 {
+	string nameOfCourse; // string to store the course name
 	GradeBook myGradeBook; // create a GradeBook object
-	myGradeBook.DisplayMessage(); //call function to display
+	
+	// prompt for and input course name
+	cout << "Please enter the course name:" << endl;
+	getline( cin, nameOfCourse ); // read a course name with blanks
+	cout << endl;
+	
+	myGradeBook.displayMessage( nameOfCourse ); //call function to display
 	return 0;
 }
